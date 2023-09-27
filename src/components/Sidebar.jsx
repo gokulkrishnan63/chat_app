7fb358513fc843b77ed5e25085603457e5827045
gokulkrishnan63 <gokul.github63@gmail.com>
2023-09-28 00:30:49 +0530

    new styles and new component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,50 @@
-import React from 'react'
+import React from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import GroupAddIcon from "@mui/icons-material/GroupAdd";
+import AddCircleIcon from "@mui/icons-material/AddCircle";
+import NightlightIcon from "@mui/icons-material/Nightlight";
+import SearchIcon from '@mui/icons-material/Search';
+import { IconButton } from "@mui/material";
+import Conversations from "./conversations";
 
 function Sidebar() {
   return (
     <div className="sidebar-container">
+      <div className="sb-header">
         <div>
-        <i class="bi bi-person-circle"></i>
-
-
+          <IconButton>
+            <AccountCircleIcon />
+          </IconButton>
         </div>
-        
-        
+        <div>
+          <IconButton>
+            <PersonAddIcon />
+          </IconButton>
+          <IconButton>
+            <GroupAddIcon />
+          </IconButton>
+          <IconButton>
+            <AddCircleIcon />
+          </IconButton>
+          <IconButton>
+            <NightlightIcon />
+          </IconButton>
         </div>
-  )
+      </div>
+      <div className="sb-search"> 
+      <IconButton>
+      <SearchIcon />
+
+      </IconButton>
+      <input type="text" placeholder="search" className="search_box"/>
+
+      </div>
+      <div className="sb-conversations">
+        <Conversations />
+         </div>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
---
+++ src/components/conversations.jsx
@@ -0,0 +1,9 @@
+
+const Conversations=()=>{
+return(
+    <>
+    </>
+)
+}
+
+export default Conversations
